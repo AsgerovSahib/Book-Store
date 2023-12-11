d026467aa83412a500b2222952bc32f7f68ece44
AsgerovSahib <asgerov.sako@gmail.com>
2023-12-12 01:14:45 +0400

    join us modal modified and other pages header and footer pasted

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -2,6 +2,13 @@
     var joinUsModalDiv = document.querySelector(".joinUs_modal_div");
     var joinUsButton = document.querySelector(".header_joinUs_Btn");
     
+
+    let joinUsBtn = document.querySelector(".joinUsBtn")
+
     joinUsButton.addEventListener("click", function () {
       joinUsModalDiv.style.display = "flex";
+    });
+    
+    joinUsBtn.addEventListener("click", function () {
+      joinUsModalDiv.style.display = "none";
     });
