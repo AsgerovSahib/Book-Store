cd387f4d7fa956db283a557f4e7098c6353a2187
narminmammadova1 <nerminytb89@gmail.com>
2023-12-11 23:07:06 +0400

    added header,footer and components html and css

---
+++ src/assets/js/header.js
@@ -0,0 +1,7 @@
+
+    var joinUsModalDiv = document.querySelector(".joinUs_modal_div");
+    var joinUsButton = document.querySelector(".header_joinUs_Btn");
+    
+    joinUsButton.addEventListener("click", function () {
+      joinUsModalDiv.style.display = "flex";
+    });
