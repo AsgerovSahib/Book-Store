f101ea3503b0d9e2ba33ad0363e86618915eb67a
Rufet Alxanov <rufat.alxanov@mail.com>
2023-12-15 00:12:26 +0400

    contact css update

---
+++ src/assets/js/contact.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const countWord=document.querySelector("#countWord");
+// const noteArea=document.querySelector("#noteArea");
+
+
+// let limit=100;
+// let count=0
+
+// let result=count + "/" + limit
+
+
+
+// noteArea.addEventListener("input",function(){
+
+// let countText=noteArea.value.length
+// let result=countText + "/" +  limit
+
+// countWord.innerHTML=result
+
+
+// if(countText > limit){
+
+
+//     countWord.style.color = "#ff2851";
+
+
+// }
+// // console.log(countWord);
+// })
+
+
+
+
