7da9384dc34fbbbcbd7135640eeb881e2e0c54be
AsgerovSahib <asgerov.sako@gmail.com>
2023-12-21 00:05:27 +0400

    created book add firebase generator part

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,12 +1,12 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getDatabase } from "firebase/database";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// import {
+//   getDatabase,
+//   set,
+//   ref,
+//   push,
+//   onValue,
+// } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// import { error, log } from "console";
-
-// // Your web app's Firebase configuration
 // const firebaseConfig = {
 //   apiKey: "AIzaSyAPS3AigURroTi0Ukx6ajBNNLJdRBHsafc",
 //   authDomain: "library-cc233.firebaseapp.com",
@@ -15,12 +15,12 @@
 //   messagingSenderId: "704000816650",
 //   appId: "1:704000816650:web:1733e6f1c3eae7bca2d1f3",
 // };
-
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 
 // // Initialize Realtime Database and get a reference to the service
 // const database = getDatabase(app);
+
 console.log("giris search js");
 //^  selector
 let admin_search_input = document.querySelector("#admin_search_input");
@@ -37,7 +37,9 @@ let about_image_url_input = document.querySelector("#about_image_url_input");
 let about_description_textarea = document.querySelector(
   "#about_description_textarea"
 );
-
+let add_book_btn = document.querySelector("#add_book_btn");
+let type_input = document.querySelector("#type_input");
+let is_new = document.querySelector("#check_input");
 //^-----------------------------------------------------------------------
 
 //*  SEARCH JS
