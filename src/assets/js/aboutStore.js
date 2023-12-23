670b655bb0a9873769bf996241a1fb8c993e0ec8
narminmammadova1 <nerminytb89@gmail.com>
2023-12-23 16:48:59 +0400

    yeni branch yaradildi

--- src/assets/js/aboutStore.js
+++ src/assets/js/aboutStore.js
@@ -1,3 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPS3AigURroTi0Ukx6ajBNNLJdRBHsafc",
+  authDomain: "library-cc233.firebaseapp.com",
+  projectId: "library-cc233",
+  storageBucket: "library-cc233.appspot.com",
+  messagingSenderId: "704000816650",
+  appId: "1:704000816650:web:1733e6f1c3eae7bca2d1f3",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -8,10 +30,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const books = document.querySelector(".books");
 
     const booksImage = books.querySelector("img");
-    const newStoretitle = localStorage.getItem("aboutTitle") || store_title.textContent
-    const newText = localStorage.getItem("aboutDesc") || text.textContent
+    // const newStoretitle = localStorage.getItem("aboutTitle") || store_title.textContent
+    // const newText = localStorage.getItem("aboutDesc") || text.textContent
 
-  const newImage = localStorage.getItem("aboutImg") || books.image.textContent;
+  // const newImage = localStorage.getItem("aboutImg") || books.image.textContent;
   
     store_title.innerHTML = newStoretitle;
     text.innerHTML = newText;
@@ -21,3 +43,24 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   });
 
+  function readData(collection) {
+    const readRef = ref(database, collection);
+    onValue(readRef, (snapshot) => {
+      const data = snapshot.val();
+      const userArr = Object.entries(data);
+const newStoretitle=user[1]
+
+
+      // let result = userArr.map(
+      //   (user, index) =>
+      //     `<tr  id="${user[1].id}}">
+      //       <th class="mobil-id">${index + 1}</th>
+      //       <th>${user[1].joinName}</th>
+      //       <th>${user[1].joinEmail}</th>
+      //     </tr>`
+      // );
+      // join_tbody.innerHTML = result.join("");
+    });
+  }
+  readData("aboutUs");
+  
--- src/assets/js/admin_about.js
+++ src/assets/js/admin_about.js
@@ -1,4 +1,27 @@
-// //* for Rufet
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPS3AigURroTi0Ukx6ajBNNLJdRBHsafc",
+  authDomain: "library-cc233.firebaseapp.com",
+  projectId: "library-cc233",
+  storageBucket: "library-cc233.appspot.com",
+  messagingSenderId: "704000816650",
+  appId: "1:704000816650:web:1733e6f1c3eae7bca2d1f3",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+
 
 
 const add_info=document.querySelector("#add_info")
@@ -11,11 +34,19 @@ add_info.addEventListener("click",function(){
     let about_image_url_input=document.querySelector("#about_image_url_input").value.trim()
    
    let about_description_textarea=document.querySelector("#about_description_textarea").value.trim()
+   const aboutUs=ref(db,"aboutUs")
 
+// localStorage.setItem("aboutTitle",about_title_input)
+// localStorage.setItem("aboutDesc",about_description_textarea)
+// localStorage.setItem("aboutImg",about_image_url_input)
 
-localStorage.setItem("aboutTitle",about_title_input)
-localStorage.setItem("aboutDesc",about_description_textarea)
-localStorage.setItem("aboutImg",about_image_url_input)
+const AboutObj={
+    about_title_input,
+    about_image_url_input,
+    about_description_textarea
+}
+set(aboutUs,AboutObj)
+
 
 document.querySelector("#about_title_input").value=""
 document.querySelector("#about_image_url_input").value=""
