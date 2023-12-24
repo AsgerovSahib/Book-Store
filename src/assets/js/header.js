712c7a0f9c4da08dee71ea71058f24b20999da70
narminmammadova1 <nerminytb89@gmail.com>
2023-12-24 09:48:21 +0400

    joinUs buttonu berpa edildi

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,4 +1,207 @@
 
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// import {
+//   getDatabase,
+//   set,
+//   ref,
+//   push,
+//   onValue,
+// } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAPS3AigURroTi0Ukx6ajBNNLJdRBHsafc",
+//   authDomain: "library-cc233.firebaseapp.com",
+//   projectId: "library-cc233",
+//   storageBucket: "library-cc233.appspot.com",
+//   messagingSenderId: "704000816650",
+//   appId: "1:704000816650:web:1733e6f1c3eae7bca2d1f3",
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+// const db = getDatabase(app);
+// ///////////////////////////////////////////////////////////////////////////////////////////
+// var joinUsModalDiv = document.querySelector(".joinUs_modal_div");
+// var joinUsButton = document.querySelector(".header_joinUs_Btn");
+// const errorDiv = document.querySelector(".errorDiv");
+// const errorMessage = document.querySelector(".errorMessage");
+
+// let joinUsBtn = document.querySelector(".joinUsBtn");
+// let mobileJoin = document.querySelector(".mobileJoin");
+
+// joinUsButton.addEventListener("click", function () {
+//   if (joinUsModalDiv.style.display === "flex") {
+//     joinUsModalDiv.style.display = "none";
+//   } else {
+//     joinUsModalDiv.style.display = "flex";
+//   }
+// });
+// mobileJoin.addEventListener("click", function () {
+//   if (joinUsModalDiv.style.display === "flex") {
+//     joinUsModalDiv.style.display = "none";
+//   } else {
+//     joinUsModalDiv.style.display = "flex";
+//   }})
+
+
+// // document.addEventListener("DOMContentLoaded", function() {
+// joinUsBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   const joinUs_fullName = document
+//     .querySelector("#joinUs_fullName")
+//     .value.trim();
+//   const joinUs_email = document.querySelector("#joinUs_email").value.trim();
+
+//   if (!joinUs_fullName || !joinUs_email) {
+//     showMessage("PLEASE WRITE YOUR USERNAME AND EMAIL", true);
+//   } else if (!joinUs_email.includes("@")) {
+//     showMessage("WRITE VALID EMAIL", true);
+//   } else {
+//     const joinObj = {
+//       joinName: joinUs_fullName,
+//       joinEmail: joinUs_email,
+//     };
+//     push(joinUs, joinObj);
+//     document.querySelector("#joinUs_fullName").value = "";
+//     document.querySelector("#joinUs_email").value = "";
+//     showMessage("Successfull process", false);
+//     joinUsModalDiv.style.display = "none";
+//     console.log("join", joinObj);
+//   }
+
+
+// });
+
+// document.addEventListener("DOMContentLoaded", function() {
+
+
+
+//   joinUsBtn.addEventListener("click",function(e){
+//     e.preventDefault()
+//       const joinUs_fullName=document.querySelector("#joinUs_fullName").value.trim()
+//       const joinUs_email=document.querySelector("#joinUs_email").value.trim()
+        
+//       if(!joinUs_fullName || !joinUs_email){
+//        showMessage("PLEASE WRITE YOUR USERNAME AND EMAIL",true)
+    
+//       }
+//       else if(!joinUs_email.includes("@")){
+//        showMessage("WRITE VALID EMAIL",true)
+//       }
+//       else{
+//         const joinObj={
+//          joinName:joinUs_fullName,
+//          joinEmail:joinUs_email
+    
+
+//     // joinUsBtn.addEventListener("click", function () {
+//     //   joinUsModalDiv.style.display = "none";
+//     // });
+
+//         }
+//         push(joinUs,joinObj)
+
+      
+        
+
+
+//         document.querySelector("#joinUs_fullName").value = ""
+//     document.querySelector("#joinUs_email").value = ""
+//         showMessage("Successfull process",false)
+//         joinUsModalDiv.style.display = "none";
+//         console.log("join",joinObj);
+//       }
+
+
+
+     
+
+
+//     })})
+
+//    function setData(collection,data){
+//     const colRef=ref(db,collection)
+//    set(colRef,data)
+//    }
+
+//    const joinUs=ref(db,"joinUs")
+
+//  function pushData(collection,data){
+
+//   const colRef=ref(db,collection)
+//   push(colRef,data)
+//  }
+
+
+//   function showMessage(message, isError) {
+//     const messageDiv = document.createElement("div");
+//     if (isError) {
+//       messageDiv.textContent = "ERROR: " + message;
+//       messageDiv.classList.add("message", "error");
+//       messageDiv.style.backgroundColor = "red";
+//       messageDiv.style.color = "white";
+//       messageDiv.style.fontSize = "16px";
+
+
+//     } else {
+//       messageDiv.textContent = message;
+//       messageDiv.classList.add("message", "success");
+//       messageDiv.style.backgroundColor = "green";
+//       messageDiv.style.color = "white";
+
+
+
+// const joinUs = ref(db, "joinUs");
+
+// function pushData(collection, data) {
+//   const colRef = ref(db, collection);
+//   push(colRef, data);
+// }
+// function setData(collection, data) {
+//   const colRef = ref(db, collection);
+//   set(colRef, data);
+// }
+
+// function showMessage(message, isError) {
+//   const messageDiv = document.createElement("div");
+//   if (isError) {
+//     messageDiv.textContent = "ERROR: " + message;
+//     messageDiv.classList.add("message", "error");
+//     messageDiv.style.backgroundColor = "red";
+//     messageDiv.style.color = "white";
+//     messageDiv.style.fontSize = "16px";
+//   } else {
+//     messageDiv.textContent = message;
+//     messageDiv.classList.add("message", "success");
+//     messageDiv.style.backgroundColor = "green";
+//     messageDiv.style.color = "white";
+//   }
+
+//   messageDiv.style.position = "fixed";
+//   messageDiv.style.top = "0";
+//   messageDiv.style.right = "0";
+//   // messageDiv.style.padding = "10px";
+//   messageDiv.style.zIndex = "9999";
+//   document.body.appendChild(messageDiv);
+//   setTimeout(function () {
+//     messageDiv.remove();
+//   }, 3000);
+// }
+
+
+// //mo
+
+// let hamburgerBtn = document.querySelector(".hamburgerBtn");
+// let sideMenu = document.querySelector(".side-menu");
+// let closeBtn = document.querySelector(".closeBtn");
+// hamburgerBtn.addEventListener("click", function () {
+//   sideMenu.style.left = "0";
+// });
+// closeBtn.addEventListener("click", function () {
+//   sideMenu.style.left = "-675px";
+
+// });
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getDatabase,
@@ -16,7 +219,6 @@ const firebaseConfig = {
   messagingSenderId: "704000816650",
   appId: "1:704000816650:web:1733e6f1c3eae7bca2d1f3",
 };
-
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
@@ -42,7 +244,7 @@ mobileJoin.addEventListener("click", function () {
   } else {
     joinUsModalDiv.style.display = "flex";
   }
-
+});
 
 // document.addEventListener("DOMContentLoaded", function() {
 joinUsBtn.addEventListener("click", function (e) {
@@ -69,90 +271,7 @@ joinUsBtn.addEventListener("click", function (e) {
     console.log("join", joinObj);
   }
 
-
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-
-
-
-  joinUsBtn.addEventListener("click",function(e){
-    e.preventDefault()
-      const joinUs_fullName=document.querySelector("#joinUs_fullName").value.trim()
-      const joinUs_email=document.querySelector("#joinUs_email").value.trim()
-        
-      if(!joinUs_fullName || !joinUs_email){
-       showMessage("PLEASE WRITE YOUR USERNAME AND EMAIL",true)
-    
-      }
-      else if(!joinUs_email.includes("@")){
-       showMessage("WRITE VALID EMAIL",true)
-      }
-      else{
-        const joinObj={
-         joinName:joinUs_fullName,
-         joinEmail:joinUs_email
-    
-
-    joinUsBtn.addEventListener("click", function () {
-      joinUsModalDiv.style.display = "none";
-    });
-
-
-
-
-        }
-        push(joinUs,joinObj)
-
-      
-        
-
-
-        document.querySelector("#joinUs_fullName").value = ""
-    document.querySelector("#joinUs_email").value = ""
-        showMessage("Successfull process",false)
-        joinUsModalDiv.style.display = "none";
-        console.log("join",joinObj);
-      }
-
-
-
-     
-
-
-    })})
-
-   function setData(collection,data){
-    const colRef=ref(db,collection)
-   set(colRef,data)
-   }
-
-   const joinUs=ref(db,"joinUs")
-
- function pushData(collection,data){
-
-  const colRef=ref(db,collection)
-  push(colRef,data)
- }
-
-
-  function showMessage(message, isError) {
-    const messageDiv = document.createElement("div");
-    if (isError) {
-      messageDiv.textContent = "ERROR: " + message;
-      messageDiv.classList.add("message", "error");
-      messageDiv.style.backgroundColor = "red";
-      messageDiv.style.color = "white";
-      messageDiv.style.fontSize = "16px";
-
-
-    } else {
-      messageDiv.textContent = message;
-      messageDiv.classList.add("message", "success");
-      messageDiv.style.backgroundColor = "green";
-      messageDiv.style.color = "white";
-
-
 
 const joinUs = ref(db, "joinUs");
 
@@ -179,7 +298,6 @@ function showMessage(message, isError) {
     messageDiv.style.backgroundColor = "green";
     messageDiv.style.color = "white";
   }
-
   messageDiv.style.position = "fixed";
   messageDiv.style.top = "0";
   messageDiv.style.right = "0";
@@ -190,7 +308,7 @@ function showMessage(message, isError) {
     messageDiv.remove();
   }, 3000);
 }
-
+// ////////////////////////////////////////
 
 //mo
 
@@ -202,6 +320,4 @@ hamburgerBtn.addEventListener("click", function () {
 });
 closeBtn.addEventListener("click", function () {
   sideMenu.style.left = "-675px";
-
 });
-
